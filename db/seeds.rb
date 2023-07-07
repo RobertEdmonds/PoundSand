5acfec80934cab790e33da7bf1361be41a76aba6
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-07-07 14:45:27 -0400

    Set data back to original

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 print("Seeding")
 print(Date.current())
 User.create!(name: "Robert Edmonds", username: "RobertEdmonds", password: "Hello123$", password_confirmation: "Hello123$", boss: true)
-User.create!(name: "Anna Omsberg", username: "AnnaOmsberg", password: "Hello123$", password_confirmation: "Hello123$", boss: true)
+User.create!(name: "Rylan Johnson", username: "RylanJohnson", password: "Hello123$", password_confirmation: "Hello123$", boss: true)
 User.create!(name: "Brian Blackburn", username: "BrianBlackburn", password: "Hello123$", password_confirmation: "Hello123$", boss: true)
 print("Seeding done")
