df6e79620590a23e6dc7c07df80174e58acece82
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-07-16 12:27:49 -0600

    fix data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :sand_useds, only: [:index, :create, :destroy]
-    resources :trucks, only: [:index, :create, :update, :destroy]
+    resources :trucks, only: [:index, :create, :update]
     resources :sites
     resources :users, only: [:index]
     # resources :company_users
